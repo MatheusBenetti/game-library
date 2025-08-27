ef8e412a3b51cd38cfb0ea74b4fa7aa63991de8f
MatheusBenetti <mbbenetti1@gmail.com>
2025-08-27 12:02:08 -0300

    Create game and find games methods implemented

--- src/main/java/com/totex/gamelibrary/infrastructure/gateway/GameRepositoryGateway.java
+++ src/main/java/com/totex/gamelibrary/infrastructure/gateway/GameRepositoryGateway.java
@@ -2,18 +2,34 @@ package com.totex.gamelibrary.infrastructure.gateway;
 
 import com.totex.gamelibrary.core.entities.Game;
 import com.totex.gamelibrary.core.gateway.GameGateway;
+import com.totex.gamelibrary.infrastructure.mapper.GameEntityMapper;
+import com.totex.gamelibrary.infrastructure.persistence.GameEntity;
+import com.totex.gamelibrary.infrastructure.persistence.GameRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class GameRepositoryGateway implements GameGateway {
+
+    private final GameRepository gameRepository;
+    private final GameEntityMapper gameEntityMapper;
+
+    public GameRepositoryGateway(GameRepository gameRepository, GameEntityMapper gameEntityMapper) {
+        this.gameRepository = gameRepository;
+        this.gameEntityMapper = gameEntityMapper;
+    }
+
     @Override
     public Game createGame(Game game) {
-        return null;
+        GameEntity entity = gameEntityMapper.toEntity(game);
+        GameEntity newGame = gameRepository.save(entity);
+        return gameEntityMapper.toDomain(newGame);
     }
 
     @Override
     public List<Game> findGames() {
-        return List.of();
+        return gameRepository.findAll().stream().map(gameEntityMapper::toDomain).toList();
     }
 
     @Override
