822f0ef6152c67067adfa7ce62d45ac3c3e740a7
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-02 18:01:38 -0300

    Create find by title in repository

--- src/main/java/com/totex/gamelibrary/infrastructure/persistence/GameRepository.java
+++ src/main/java/com/totex/gamelibrary/infrastructure/persistence/GameRepository.java
@@ -1,6 +1,12 @@
 package com.totex.gamelibrary.infrastructure.persistence;
 
+import com.totex.gamelibrary.core.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GameRepository extends JpaRepository<GameEntity, Long> {
+    Optional<Game> findByTitleIgnoreCase(String title);
+
+
 }
